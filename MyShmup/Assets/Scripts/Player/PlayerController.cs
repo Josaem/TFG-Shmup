6659f1fafad0022f4e50857aceacbddd5c4a703d
Josaem <82065172+Josaem@users.noreply.github.com>
2023-02-03 23:54:39 +0100

    Added Player Shot

--- MyShmup/Assets/Scripts/Player/PlayerController.cs
+++ MyShmup/Assets/Scripts/Player/PlayerController.cs
@@ -10,7 +10,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _fastSpeed;
     [SerializeField] private float _slowSpeed;
     private bool _slowMovement = false;
-    private bool _fire = false;
     private bool _invincible = false;
     [SerializeField] private float _blockTime;
     [SerializeField] private float _blockCoolDown;
@@ -35,6 +34,18 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer _hitboxRenderer;
     [SerializeField] private Transform _playerRespawnLocation;
 
+    [SerializeField] private float _fireTime;
+    private float _fireRemainingCooldown;
+    [SerializeField] private float _fireRate;
+    private float _timeToShoot = 0;
+    private bool _fireButtonPressed = false;
+    [SerializeField] private float _timeToLaser;
+    private float _timeUntilLaser;
+
+    [SerializeField] private Transform _playerBulletPool;
+    [SerializeField] private GameObject _playerShotVulcan;
+    [SerializeField] private Transform[] _bulletSpawnLocation;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -52,12 +63,13 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        fireAction.performed += ctx => {
-            _fire = true;
-            _slowMovement = true;
+        _timeUntilLaser = _timeToLaser;
+
+        fireAction.started += ctx => {
+            StartShooting();
         };
-        fireAction.canceled += ctx => { _fire = false;
-            _slowMovement = false;
+        fireAction.canceled += ctx => {
+            CancelShooting();
         };
 
         blockAction.performed += ctx => {
@@ -67,28 +79,68 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        //Decrease shield cooldown
         if(_shieldRemainingCooldown > 0) _shieldRemainingCooldown = Mathf.Max(_shieldRemainingCooldown - Time.deltaTime, 0f);
+
+        //Change hitbox color depending on shield cooldown
         if (_shieldRemainingCooldown == 0 && _hitboxRenderer.color != _shieldNotOnCooldownColor) _hitboxRenderer.color = _shieldNotOnCooldownColor;
+
+        //Decrease time where fire is active 
+        if (_fireRemainingCooldown > 0) _fireRemainingCooldown = Mathf.Max(_fireRemainingCooldown - Time.deltaTime, 0f);
+        if (_fireRemainingCooldown > 0 && _timeUntilLaser > 0) Fire();
+
+        //Decrease time where fire is active 
+        if (_timeUntilLaser > 0 && _fireButtonPressed) _timeUntilLaser = Mathf.Max(_timeUntilLaser - Time.deltaTime, 0f);
+        if (_timeUntilLaser == 0 && _fireButtonPressed) FireLaser();
     }
 
     private void FixedUpdate()
     {
         Move();
-
-        if(_fire) Fire();
     }
 
     private void Move()
     {
+        //Slow ship if shooting
         if (_slowMovement)
             _rb.velocity = _movement * _slowSpeed * Time.deltaTime;
         else
             _rb.velocity = _movement * _fastSpeed * Time.deltaTime;
     }
 
+    private void StartShooting()
+    {
+        _fireRemainingCooldown = _fireTime;
+        _fireButtonPressed = true;
+        _timeUntilLaser = _timeToLaser;
+    }
+
     private void Fire()
     {
+        //We have a cooldown for each keypress, which in turn shoots shots in a burst
+        //TODO add burst limit to 1 per keypress
+        if(Time.time > _timeToShoot)
+        {
+            _timeToShoot = Time.time + _fireRate;
 
+            foreach (Transform gun in _bulletSpawnLocation)
+            {
+                Instantiate(_playerShotVulcan,
+                    gun.position, gun.rotation,
+                    _playerBulletPool);
+            }
+        }
+    }
+
+    private void FireLaser()
+    {
+        if(_slowMovement == false) _slowMovement = true;
+    }
+
+    private void CancelShooting()
+    {
+        _slowMovement = false;
+        _fireButtonPressed = false;
     }
 
     private void Block()
