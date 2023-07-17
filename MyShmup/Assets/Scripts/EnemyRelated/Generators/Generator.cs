154796540277469b65feb57d47b2737bfbcfe85d
Josaem <82065172+Josaem@users.noreply.github.com>
2023-07-17 20:45:03 +0200

    Fixed Wave11 not spawning

--- MyShmup/Assets/Scripts/EnemyRelated/Generators/Generator.cs
+++ MyShmup/Assets/Scripts/EnemyRelated/Generators/Generator.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Generator : MonoBehaviour
 {
